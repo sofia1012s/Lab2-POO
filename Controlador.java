432468044bef9c815a28b7cdfcd79b73ccb86893
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-09-14 19:02:57 -0600

    Update Controlador.java

--- Controlador.java
+++ Controlador.java
@@ -1,3 +1,23 @@
 public class Controlador {
-    
+    public static void main(String[] args) {
+        Vista view = new Vista();
+        Programa programa;
+        RAM ram;
+
+        view.bienvenida();
+        int opcion = 0;
+
+        while (opcion != 11) {
+            opcion = view.menuOpciones(opcion);
+            
+            switch (opcion) {
+                case 1:
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    }
 }
